8908128036db784719237c067dc964c2436f923e
Balazs Nemeth <balazs.1.nemeth@nokia.com>
2017-12-15 16:07:03 +0100

    add onclick handler for toggleing todo

--- src/index.js
+++ src/index.js
@@ -28,8 +28,19 @@ class App extends Component {
         Add todo
       </button>
       <ul>
-        {this.props.todos.map(
-          todo => <li key={todo.id}>{todo.text}</li>)
+        {this.props.todos.map( todo =>
+          <li key={todo.id} 
+            onClick={ () => {
+              store.dispatch({
+                type: 'TOGGLE_TODO',
+                id: todo.id
+              })
+            }} 
+            style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+          >
+            {todo.text}
+          </li>
+          )
         }
       </ul>
     </div>
