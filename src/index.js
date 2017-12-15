2ff1f818faae745cecb2a5f227e800675028b24d
csabagaspar <gaspar.csaba@gmail.com>
2017-12-15 14:14:02 +0000

    add input and button

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {render} from 'react-dom'
 import {todo, todos, visibilityFilter} from './reducers'
 import {createStore, combineReducers} from 'redux'
@@ -8,11 +8,36 @@ const reducer = combineReducers({
   visibilityFilter
 })
 const store = createStore(reducer)
-const App = () => {
-  return (
-    <div>
-      Hello
+let nextId = 0
+class App extends Component {
+  render(){
+
+    return (
+      <div>
+        <input ref={(input) => this.input = input}/>
+        <button onClick={() => {
+          store.dispatch({
+            id: nextId++,
+            type: 'ADD_TODO',
+            text: this.input.value
+          })
+          this.input.value = ''
+        }
+        }
+      >
+        Add todo
+      </button>
+      <ul>
+        {this.props.todos.map(
+          todo => <li key={todo.id}>{todo.text}</li>)
+        }
+      </ul>
     </div>
-  )
+    )
+  }
 }
-render(<App/>, document.getElementById('root'))
+const renderApp = () => {
+  render(<App todos={store.getState().todos} />, document.getElementById('root'))
+}
+store.subscribe(renderApp)
+renderApp()
